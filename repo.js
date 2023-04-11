c584d4a5af19d5a89864e2c081a2de5f2dce73e6
Ivy-aminga <amingaivy@gmail.com>
2023-04-11 19:31:41 +0300

    first commit

---
+++ repo.js
@@ -0,0 +1,17 @@
+// // Write a function that takes a string as input and returns the length of the string.
+function take(str){
+    return str.length
+ }
+ let myString = "Ivy Aming'a";
+ let inString = take (myString);
+ console.log(inString);
+
+// Write a function that takes a string as input and returns the string in uppercase letters.
+
+function toUpperCase(str) { 
+    return str.toUpperCase();
+  }
+  let topUpper = "Ivy Aming'a";
+let upperCaseString = toUpperCase(topUpper);
+console.log(upperCaseString);
+  
