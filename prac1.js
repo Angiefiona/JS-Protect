a9a64a4a597895aab214e1de24d8ec1e0cdfe691
Angiefiona <124134947+Angiefiona@users.noreply.github.com>
2023-04-12 20:54:58 +0300

    Add files via upload

---
+++ prac1.js
@@ -0,0 +1,13 @@
+//Using JavaScript, find the following words from the following strings:
+//1. "market"
+//const string1 = "The lady went to the market with her sister";
+//2. "season"
+//const string2 = "My favorite season is spring"
+
+const string1 = "The lady went to the market with her sister";
+const index = string1.indexOf("market");
+console.log(index)
+
+const string2 = "My favorite season is spring";
+const index1 = string2.indexOf("season");
+console.log(index1)
