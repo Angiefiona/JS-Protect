246012e1807057547881e794ded6d3f4b8052deb
Ivy-aminga <amingaivy@gmail.com>
2023-04-12 10:10:32 +0300

    JS protect

--- repo.js
+++ repo.js
@@ -15,3 +15,40 @@ function toUpperCase(str) {
 let upperCaseString = toUpperCase(topUpper);
 console.log(upperCaseString);
   
+
+// Write a function that takes a string as input and returns the string in lowercase letters.
+function toLowerCase(str){
+  return str.toLowerCase();
+}
+let toLower = "IVY AMING'A";
+let loweCasesString = toLowerCase(toLower);
+console.log(loweCasesString);
+// Write a function that takes a string as input and returns the first character of the string.
+function firstChar(str) {
+  return str.charAt(0);
+}
+console.log(firstChar("Aming'a")); 
+
+// Write a function that takes a string as input and returns the last character of the string.
+function lastIndexOf(str){
+  return str.charAt(str.length -1);
+}
+console.log(lastIndexOf("Aming'a"));
+
+// Write a function that takes two strings as input and concatenates them together.
+function concatenateStrings(str1, str2) {
+  return str1 + str2;
+}
+let str1 = "Ivy";
+let str2 ="Aming'a";
+console.log(str1, str2);
+// Write a function that takes two strings as input and checks if they are equal.
+
+function equalSrtrings(str1, str2) {
+  return str1 === str2;
+}
+let string1 = 'Ivy';
+let string2 = "AMing'a";
+
+
+console.log(equalSrtrings(string1, string2)); 
