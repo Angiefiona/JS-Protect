496a88628e9f3516d71cf4b1e6d9cb913901a7d1
Angiefiona <124134947+Angiefiona@users.noreply.github.com>
2023-04-13 18:14:08 +0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,11 @@
+// Find the last element of the following arrays.
+// arr1 = [3,7,34,90,12]
+// arr2 = [true, "green", "where",12,56]
+
+arr1 = [3,7,34,90,12];
+let lastElement = arr1[arr1.length -1];
+console.log(lastElement)
+
+arr2 = [true, "green", "where",12,56];
+let lastElement1 = arr2[arr2.length -1];
+console.log(lastElement1)
