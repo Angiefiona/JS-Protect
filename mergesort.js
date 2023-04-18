0aed2bb58a95cc5087307a65ecb8f48c4de72f92
nyeliep <gielpalnyeliep@gmail.com>
2023-04-18 21:58:08 +0300

    Add files via upload

--- mergesort.js
+++ mergesort.js
@@ -24,3 +24,32 @@ function sortedArray(left,right){
 
 let arr = [2,13,32,50,11,5,6];
 console.log(divideArray(arr));
+
+
+
+function merge(num){
+  if(num.length <= 1){
+    return num;
+  }
+  let middle = Math.floor(num.length/2);
+  let start = num.slice(0, middle);
+  let end = num.slice(middle);
+  return sort(merge(start),merge(end))
+}
+
+function sort(start, end){
+  let emptyNum = []
+  while(start.length && end.length){
+    if(start[0] < end[0]){
+      emptyNum.push(start.shift());
+    }
+    else{
+      emptyNum.push(end.shift())
+    }
+  }
+  return [...emptyNum,...start,...end]
+
+}
+
+let num =  [34,56,12,8,9,23,41,4,36,17]
+console.log(merge(num));
